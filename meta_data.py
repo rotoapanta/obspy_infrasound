4e26b22507db04da57fc1c7dd9b6944f44477395
Roberto Toapanta-Guamán <robertocarlos.toapanta@gmail.com>
2025-01-09 16:45:15 -0500

    Update meta_data.py

--- meta_data.py
+++ meta_data.py
@@ -3,14 +3,15 @@ import obspy
 # Leer el archivo MiniSEED
 st = obspy.read("/home/rotoapanta/Documentos/DiGOS/DTA_CEDIA/datos_bnas_lahar/EC.BNAS..BHZ.D.2016.013")
 
-# Recorrer cada trazo (trace) en el Stream
-for tr in st:
-    print("Código de estación (station):", tr.stats.station)
-    print("Código de red (network):", tr.stats.network)
-    print("Código de canal (channel):", tr.stats.channel)
-    print("Código de ubicación (location):", tr.stats.location)
-    print("Hora de inicio (starttime):", tr.stats.starttime)
-    print("Hora de fin (endtime):", tr.stats.endtime)
-    print("Sampling rate:", tr.stats.sampling_rate)
-    print("Número de muestras (npts):", tr.stats.npts)
-    print("------------------------------------------------------")
+# Obtener el primer trazo
+tr = st[0]
+
+# Imprimir la información del primer trazo
+print(f"Código de estación (station): {tr.stats.station}")
+print(f"Código de red (network): {tr.stats.network}")
+print(f"Código de canal (channel): {tr.stats.channel}")
+print(f"Código de ubicación (location): {tr.stats.location}")
+print(f"Hora de inicio (starttime): {tr.stats.starttime}")
+print(f"Hora de fin (endtime): {tr.stats.endtime}")
+print(f"Sampling rate: {tr.stats.sampling_rate}")
+print(f"Número de muestras (npts): {tr.stats.npts}")
