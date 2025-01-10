1f1d2a94d97e10bfee312188d241d67df3952d30
Roberto Toapanta-Guamán <robertocarlos.toapanta@gmail.com>
2025-01-10 08:48:55 -0500

    Update one-day_plot.py

--- one-day_plot.py
+++ one-day_plot.py
@@ -5,14 +5,19 @@ archivo = "/home/rotoapanta/Documentos/DiGOS/DTA_CEDIA/datos_bnas_lahar/EC.BNAS.
 
 # Leer el archivo de datos
 st = read(archivo)
-#st.filter("lowpass", freq=0.1, corners=2)
+
+# Filtrar la señal utilizando un filtro pasa-banda entre 1.0 Hz y 3.0 Hz.
+# 'freqmin' establece la frecuencia mínima (1.0 Hz) y 'freqmax' la frecuencia máxima (3.0 Hz).
+# 'corners' define el número de segmentos del filtro (4 en este caso, para un filtro de orden 4).
+# 'zerophase' asegura que el filtro no introduce desfase en la señal (filtro de fase cero).
 st.filter('bandpass', freqmin=1.0, freqmax=3.0, corners=4, zerophase=True)
 
-# Graficar los datos
-#st.plot(type="dayplot", interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
-#        one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=False,
-#        events={'min_magnitude': 4.5})
-
-
-st.plot(type="dayplot", interval=60, right_vertical_labels=False, vertical_scaling_range=5e3,
+# Graficar los datos de ObsPy utilizando 'dayplot' (gráfico por día).
+# 'interval' controla el intervalo entre las marcas de tiempo en la gráfica (60 minutos en este caso).
+# 'right_vertical_labels' si es True, agrega etiquetas en el eje Y a la derecha de la gráfica.
+# 'vertical_scaling_range' ajusta el rango de amplitud en el eje Y. Se usa 5e3 como un valor de ejemplo.
+# 'one_tick_per_line' asegura que haya una marca (tick) por cada línea de datos.
+# 'color' especifica los colores para las trazas. ['k', 'r', 'b', 'g'] corresponde a negro, rojo, azul y verde.
+# 'show_y_UTC_label' si es True, muestra las etiquetas UTC (hora en formato UTC) en el eje Y.
+st.plot(type="dayplot", interval=60, right_vertical_labels=True, vertical_scaling_range=5e3,
         one_tick_per_line=True, color=['k', 'r', 'b', 'g'], show_y_UTC_label=True)
